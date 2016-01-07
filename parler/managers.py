2a9bf28b2bdac9b3d99d43ee1025f19884233660
Pawel Markowski <p.markowski@getprintbox.com>
2016-01-07 14:13:17 +0100

    Manager method - translated, not ready yet

--- parler/managers.py
+++ parler/managers.py
@@ -4,14 +4,14 @@ Custom generic managers
 import django
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
-from django.db.models.query import QuerySet
+from django.db.models.query import QuerySet, Q
 from django.utils.translation import get_language
 from django.utils import six
 from parler import appsettings
 from parler.utils import get_active_language_choices
 
 
-class TranslatableQuerySet(QuerySet):
+class TranslatableQuerySetDefault(QuerySet):
     """
     An enhancement of the QuerySet which sets the objects language before they are returned.
 
@@ -20,12 +20,12 @@ class TranslatableQuerySet(QuerySet):
     """
 
     def __init__(self, *args, **kwargs):
-        super(TranslatableQuerySet, self).__init__(*args, **kwargs)
+        super(TranslatableQuerySetDefault, self).__init__(*args, **kwargs)
         self._language = None
 
 
     def _clone(self, klass=None, setup=False, **kw):
-        c = super(TranslatableQuerySet, self)._clone(klass, setup, **kw)
+        c = super(TranslatableQuerySetDefault, self)._clone(klass, setup, **kw)
         c._language = self._language
         return c
 
@@ -35,7 +35,7 @@ class TranslatableQuerySet(QuerySet):
         # like .language('xx').create(..) which is a nice API after all.
         if self._language:
             kwargs['_current_language'] = self._language
-        return super(TranslatableQuerySet, self).create(**kwargs)
+        return super(TranslatableQuerySetDefault, self).create(**kwargs)
 
 
     def language(self, language_code=None):
@@ -108,13 +108,76 @@ class TranslatableQuerySet(QuerySet):
         # Based on django-queryset-transform.
         # This object however, operates on a per-object instance
         # without breaking the result generators
-        base_iterator = super(TranslatableQuerySet, self).iterator()
+        base_iterator = super(TranslatableQuerySetDefault, self).iterator()
         for obj in base_iterator:
             # Apply the language setting.
             if self._language:
                 obj.set_current_language(self._language)
 
             yield obj
+
+
+class JSONTranslatableQuerySet(TranslatableQuerySetDefault):
+
+    def translated(self, *language_codes, **translated_fields):
+        """
+        Only return translated objects which of the given languages.
+
+        When no language codes are given, only the currently active language is returned.
+
+        .. note::
+
+            Due to Django `ORM limitations <https://docs.djangoproject.com/en/dev/topics/db/queries/#spanning-multi-valued-relationships>`_,
+            this method can't be combined with other filters that access the translated fields. As such, query the fields in one filter:
+
+            .. code-block:: python
+
+                qs.translated('en', name="Cheese Omelette")
+
+            This will query the translated model for the ``name`` field.
+        """
+        translations_fields = \
+            self.model._parler_meta.get_all_translations_fields()
+
+        if not language_codes:
+            language_codes = (get_language(),)
+
+        filters = {}
+
+        if translated_fields:
+            return self
+        else:
+            if len(language_codes) == 1:
+                lang_filter = ('__has', language_codes[0])
+            else:
+                # TODO or has_all
+                lang_filter = ('__has_any', language_codes)
+
+            # Final prefixing
+            for name in translations_fields:
+                filters[name + lang_filter[0]] = lang_filter[1]
+            query = reduce(lambda q, f: q | Q(f), filters, Q())
+            return self.filter(query)
+
+
+        # for field_name, val in six.iteritems(translated_fields):
+        #     if field_name.startswith('master__'):
+        #         filters[field_name[8:]] = val  # avoid translations__master__ back and forth
+        #     else:
+        #         filters["{0}__{1}".format(relname, field_name)] = val
+        #
+        # if len(language_codes) == 1:
+        #     filters[relname + '__language_code'] = language_codes[0]
+        #     return self.filter(**filters)
+        # else:
+        #     filters[relname + '__language_code__in'] = language_codes
+        #     return self.filter(**filters).distinct()
+
+
+if appsettings.PARLER_BACKEND == 'json':
+    TranslatableQuerySet = JSONTranslatableQuerySet
+else:
+    TranslatableQuerySet = TranslatableQuerySetDefault
 
 
 class TranslatableManager(models.Manager):
